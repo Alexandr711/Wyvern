fdbc097cb197b784883288dc5a3cc0c42fe02f46
Alexandr711 <aleksandrfilippov711@gmail.com>
2025-08-17 17:51:45 +0300

    Changes for Ui.cpp and Ui.h

--- Src/Ui/Ui.cpp
+++ Src/Ui/Ui.cpp
@@ -2,7 +2,29 @@
 
 Gui::Gui()
 {
+    mainWindow = new MainWindow(this);
 
+    menuBar = new QMenuBar(this);
+    fileMenu = new FileMenu(this);
+
+    mainToolBar = new MainToolBar(this);
 }
 
 Gui::~Gui(){}
+
+void Gui::setItems()
+{
+    mainWindow->setMenuBar(menuBar);
+    setMenuBarItems();
+    fileMenu->setItems();
+
+    mainWindow->addToolBar(mainToolBar);
+
+
+}
+
+void Gui::setMenuBarItems()
+{
+    menuBar->addMenu(fileMenu);
+}
+
--- Src/Ui/Ui.h
+++ Src/Ui/Ui.h
@@ -1,12 +1,33 @@
 #ifndef UI_H
 #define UI_H
 
+#include <QMenuBar>
+
+#include "MainWindow/MainwWndow.h"
+#include "MenuBar/FileMenu.h"
+#include "ToolBars/MainToolBar.h"
 
 class Gui
 {
 public:
     Gui();
     ~Gui();
+
+    void setItems();
+
+private:
+    void setMenuBarItems();
+
+private:
+    MainWindow* mainWindow;
+
+    //MenuBar
+    QMenuBar* menuBar;
+    FileMenu* fileMenu;
+
+    //ToolBars
+    MainToolBar* mainToolBar;
+
 };
 
 #endif // UI_H
